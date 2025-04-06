5bf777ebbda1f5fa4c8479b1d6d206dfab46f7d7
lucas reis <lucasreis-@outlook.com>
2025-04-06 12:53:16 -0300

    Criação do projeto

---
+++ src/test/java/com/luecardoso/ProjetoSpring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luecardoso.ProjetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
