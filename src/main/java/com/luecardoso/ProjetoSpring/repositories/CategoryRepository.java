979b5eb1d7310e2ddf3ab5d46142b31a0a4fff2f
lucas reis <lucasreis-@outlook.com>
2025-04-06 21:02:37 -0300

    Adicionando a classe Categoria
    -implementando status do pedido
    
    -populando no banco

---
+++ src/main/java/com/luecardoso/ProjetoSpring/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.luecardoso.ProjetoSpring.repositories;
+
+import com.luecardoso.ProjetoSpring.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/luecardoso/ProjetoSpring/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.luecardoso.ProjetoSpring.services;
+
+import com.luecardoso.ProjetoSpring.entities.Category;
+import com.luecardoso.ProjetoSpring.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
