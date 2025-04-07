dc4a769c362f98e6ae5fff9c2f5b7b425bbead74
lucas reis <lucasreis-@outlook.com>
2025-04-07 12:02:41 -0300

    Adicionando classe Produtos
    -mapeando relacionamento com Categorias
    
    -populando no banco

--- src/main/java/com/luecardoso/ProjetoSpring/entities/Category.java
+++ src/main/java/com/luecardoso/ProjetoSpring/entities/Category.java
@@ -1,8 +1,11 @@
 package com.luecardoso.ProjetoSpring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +16,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -37,6 +44,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/luecardoso/ProjetoSpring/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.luecardoso.ProjetoSpring.repositories;
+
+import com.luecardoso.ProjetoSpring.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
